3eceffc3608ebac3a06d9564e0b3db8acee33081
Arne Welzel <arne.welzel@corelight.com>
2022-03-12 14:58:52 +0100

    Bump default port to 9916.

--- main.go
+++ main.go
@@ -561,7 +561,7 @@ var (
 	version     string // Set via -ldflags -X main.version=...
 	showVersion = flag.Bool("version", false, "Output version information.")
 	socketPath  = flag.String("suricata.socket-path", "/var/run/suricata.socket", "Path to the Suricata Command socket.")
-	addr        = flag.String("web.listen-address", ":9915", "Address to listen on")
+	addr        = flag.String("web.listen-address", ":9916", "Address to listen on")
 )
 
 func main() {
